58ea2ab455e004717880b14c9cee990be05d0884
andrew <admin@AC-HOME>
2021-05-21 10:53:33 +1000

    Changed gulp clean directory. Add check for docker MYSQL in simple.spec.ts

--- test/simple.spec.ts
+++ test/simple.spec.ts
@@ -1,9 +1,31 @@
 'use strict';
 
+import {expect} from 'chai';
+import * as Docker from 'dockerode';
+import * as os from 'os';
+
 describe('Test - DO nothing', () => {
 
   it(`Pass test Case`, () => {
     return;
   });
 
+});
+
+
+describe('Check docker MySQL is running', () => {
+
+  it(`Check container exists test Case`, () => {
+
+    const isWin = os.type() === 'Windows_NT';
+    const socket = process.env.DOCKER_SOCKET || isWin ? '//./pipe/docker_engine' : '/var/run/docker.sock';
+    const docker = new Docker({socketPath: socket});
+    const container = docker.getContainer('mysql');
+    const container_opts = {stream: true, stdin: true, stdout: true, stderr: true};
+
+    container.stats(container_opts, function(err, stream) {
+      expect(err).to.be.null;
+      expect(stream.pipe).to.be.ok;
+    });
+  });
 });
